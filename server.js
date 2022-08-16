1036ec2be9a608f06ea51d6d248bd5e437f995a1
Nio <nio@example.com>
2022-08-16 15:23:25 +0300

    commit

--- server.js
+++ server.js
@@ -1,18 +1,34 @@
-function requireHTTPS(req, res, next) {
-    // The 'x-forwarded-proto' check is for Heroku
-    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
-        return res.redirect('https://' + req.get('host') + req.url);
-    }
-    next();
-}
+// function requireHTTPS(req, res, next) {
+//     // The 'x-forwarded-proto' check is for Heroku
+//     if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+//         return res.redirect('https://' + req.get('host') + req.url);
+//     }
+//     next();
+// }
 
 
+// const express = require('express');
+// const app = express();
+// app.use(requireHTTPS);
+// app.use(express.static('./dist/todoApp'));
+// app.get('/*', function(req, res) {
+//   res.sendFile(`index.html`, {root: 'dist/todoApp/'}
+// );
+// });
+// app.listen(process.env.PORT || 8080);
+
+//Install express server
 const express = require('express');
+const path = require('path');
+
 const app = express();
-app.use(requireHTTPS);
+
+// Serve only the static files form the dist directory
 app.use(express.static('./dist/todoApp'));
-app.get('/*', function(req, res) {
-  res.sendFile(`index.html`, {root: 'dist/todoApp/'}
+
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/todoApp/'}),
 );
-});
+
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
